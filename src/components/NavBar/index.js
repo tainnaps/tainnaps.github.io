ae37d63f0973b3cbc4b3ffa8d966b98a50d0fc0f
Tainá Santana <tainnaps@gmail.com>
2022-03-22 04:51:05 -0300

    feat: style nav bar

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './index.css';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <Link className="home-link" to="/">{ '<Tainá Santana>' }</Link>
+      <Link to="/about">About</Link>
+      <Link to="/projects">Projects</Link>
+      <a
+        href="https://github.com/tainnaps"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Github
+      </a>
+      <a
+        href="https://www.linkedin.com/in/tainnaps/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Linkedin
+      </a>
+    </nav>
+  );
+}
+
+export default NavBar;
