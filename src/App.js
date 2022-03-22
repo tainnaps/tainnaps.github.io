e1d60c8fc61051048911d01119a17a030b4b5d63
Tainá Santana <tainnaps@gmail.com>
2022-03-22 00:47:26 -0300

    feat: create portfolio routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Projects from './pages/Projects';
+import About from './pages/About';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Home } />
+      <Route path="/projects" component={ Projects } />
+      <Route path="/about" component={ About } />
     </Switch>
   );
 }
