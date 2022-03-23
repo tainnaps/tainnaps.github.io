aa7b23d25cd5e5c43357ed6cfc320e90fb78bc3a
Tainá Santana <tainnaps@gmail.com>
2022-03-23 03:11:15 -0300

    feat: create technologies component

---
+++ src/components/Technologies/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './index.css';
+
+function Technologies() {
+  return (
+    <section className="technologies-section">
+      <h1>Technologies</h1>
+      <section className="technologies">
+        <i className="fa-brands fa-html5 tech-icons"></i>
+        <i className="fa-brands fa-css3-alt tech-icons"></i>
+        <i className="fa-brands fa-js tech-icons"></i>
+        <i className="fa-brands fa-react tech-icons"></i>
+        <i className="fa-brands fa-docker tech-icons"></i>
+        <i className="fa-solid fa-database tech-icons"></i>
+        <i className="fa-brands fa-node tech-icons"></i>
+      </section>
+    </section>
+  );
+}
+
+export default Technologies;
