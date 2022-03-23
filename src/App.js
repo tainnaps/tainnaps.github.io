b446e6cf971f9cae6b2a031b3fee61a603b8dede
Tainá Santana <tainnaps@gmail.com>
2022-03-23 03:43:07 -0300

    feat: create technologies and contact routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import About from './pages/About';
 import NavBar from './components/NavBar';
+import Technologies from './pages/Technologies';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route exact path="/" component={ Home } />
         <Route path="/projects" component={ Projects } />
         <Route path="/about" component={ About } />
+        <Route path="/technologies" component={ Technologies } />
+        <Route path="/contact" component={ Contact } />
       </Switch>
     </div>
   );
