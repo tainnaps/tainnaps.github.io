2819a39478682f718e9bafa5b842262b62a80fa2
Tainá Santana <tainnaps@gmail.com>
2022-03-24 01:25:36 -0300

    refactor: split description in more paragraphs

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Footer from '../../components/Footer';
 import PersonalPicture from '../../components/PersonalPicture';
 import './index.css';
 
@@ -12,14 +11,17 @@ function About() {
           <p>
             Hello! I am Tainá, a brazilian Web Software Developer.
             My journey with programming has began throught electronics, when I used to write programs to microcontrollers using Assembly and C programming languages.
+          </p>
+          <p>
             Now days, I build web applications using React.js to the front-end and Node.js to the back-end.
             I am currently studying at Trybe and improving myself to develop applications that will be useful to other people.
+          </p>
+          <p>
             Feel free to see the projects I've done and contact me. Have a nice time!
           </p>
         </section>
         <PersonalPicture />
       </main>
-      <Footer />
     </>
   );
 }
