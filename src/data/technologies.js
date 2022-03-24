8472c2446b1c783db2c0f7667966f91372157611
Tainá Santana <tainnaps@gmail.com>
2022-03-24 01:23:22 -0300

    feat: add technologies array

---
+++ src/data/technologies.js
@@ -0,0 +1,52 @@
+const technologies = [
+  {
+    name: 'HTML',
+    imgSrc: 'https://icons.iconarchive.com/icons/cornmanthe3rd/plex/256/Other-html-5-icon.png',
+  },
+  {
+    name: 'CSS',
+    imgSrc: 'https://logospng.org/download/css-3/logo-css-3-2048.png',
+  },
+  {
+    name: 'JavaScript',
+    imgSrc: 'https://logospng.org/download/javascript/logo-javascript-icon-1024.png',
+  },
+  {
+    name: 'Jest',
+    imgSrc: 'https://symbols.getvecta.com/stencil_25/40_jest.5fde12ec22.png',
+  },
+  {
+    name: 'React',
+    imgSrc: 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png',
+  },
+  {
+    name: 'Redux',
+    imgSrc: 'https://img.icons8.com/color/480/redux.png',
+  },
+  {
+    name: 'Docker',
+    imgSrc: 'https://cdn-icons-png.flaticon.com/512/919/919853.png',
+  },
+  {
+    name: 'MySQL',
+    imgSrc: 'https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-images-are-download-crazypng-21.png',
+  },
+  {
+    name: 'Node',
+    imgSrc: 'https://walde.co/wp-content/uploads/2016/09/nodejs_logo.png',
+  },
+  {
+    name: 'Express',
+    imgSrc: 'https://freepikpsd.com/file/2019/10/express-js-png-5-Transparent-Images.png',
+  },
+  {
+    name: 'Mocha',
+    imgSrc: 'https://cldup.com/xFVFxOioAU.svg',
+  },
+  {
+    name: 'Chai',
+    imgSrc: 'https://avatars.githubusercontent.com/u/1515293?s=280&v=4',
+  },
+];
+
+export default technologies;
--- src/pages/Technologies/index.js
+++ src/pages/Technologies/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './index.css';
+import technologies from '../../data/technologies';
 
 function Technologies() {
   return (
     <section className="technologies-section">
       <h1>Technologies</h1>
       <section className="technologies">
-        <i className="fa-brands fa-html5 tech-icons"></i>
-        <i className="fa-brands fa-css3-alt tech-icons"></i>
-        <i className="fa-brands fa-js tech-icons"></i>
-        <i className="fa-brands fa-react tech-icons"></i>
-        <i className="fa-brands fa-docker tech-icons"></i>
-        <i className="fa-solid fa-database tech-icons"></i>
-        <i className="fa-brands fa-node tech-icons"></i>
+        {
+          technologies.map(({ name, imgSrc }) => (
+            <div key={ name } className="tech-card">
+              <img className="tech-logo" src={ imgSrc } alt={ name } />
+              <h3 className="tech-name">{ name }</h3>
+            </div>
+          ))
+        }
       </section>
     </section>
   );
