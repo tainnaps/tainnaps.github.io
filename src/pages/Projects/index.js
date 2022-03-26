9ae85469376adca3a99cce1cd575e88ca4de249a
Tainá Santana <tainnaps@gmail.com>
2022-03-26 04:52:25 -0300

    feat: add project filter structure

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import technologies from '../../data/technologies';
 import './index.css';
 
 function Projects() {
   return (
-    <main className="projects">
-      <h1>In progress</h1>
+    <main className="projects-content">
+      <section className="right">
+        <section className="projects-title">
+          <h1>Projects</h1>
+          <hr />
+        </section>
+        <section className="projects-search">
+          <p>Filter projects by technologies</p>
+          <section className="tech-filter">
+            {
+              technologies.map(({ name }) => (
+                <label
+                  htmlFor={ name }
+                  key={ name }
+                  className="tech-label"
+                >
+                  <input
+                    type="checkbox"
+                    id={ name }
+                    name={ name }
+                  />
+                  { name }
+                </label>
+              ))
+            }
+          </section>
+        </section>
+      </section>
+      <section className="left"></section>
     </main>
   );
 }
