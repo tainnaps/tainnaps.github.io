2717d88ad472dcfba18d27d74d35d5e5d71d3f83
Tainá Santana <tainnaps@gmail.com>
2022-03-26 04:49:05 -0300

    feat: create projects data file

---
+++ src/data/projects.js
@@ -0,0 +1,11 @@
+const projects = [
+  {
+    name: 'To Do List',
+    imgSrc: 'https://64.media.tumblr.com/ad99ca94758999b3c31ef5c4502479da/688d378c08c29ab4-97/s2048x3072/cf24fdadc8ac7aac9534043e4fc44dd0cde2b427.png',
+    application: 'https://tainnaps.github.io/to-do-list/',
+    repository: 'https://github.com/tainnaps/to-do-list',
+    description: 'This is a simple To Do List application where the user can insert, mark as completed, move and delete tasks. Furthermore, it is possible to save and delete the whole list or delete only the completed tasks.',
+  }
+];
+
+export default projects;
