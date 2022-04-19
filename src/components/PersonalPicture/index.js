12e551e59c4b275f035c891fc5cd55e65d715865
Tainá Santana <tainnaps@gmail.com>
2022-04-19 04:22:13 -0300

    fix: change personal picture

--- src/components/PersonalPicture/index.js
+++ src/components/PersonalPicture/index.js
@@ -4,7 +4,7 @@ import './index.css';
 function PersonalPicture() {
   return (
     <section>
-      <img className="personal-picture" src="../../images/taina.jpg" alt="Tainá" />
+      <img className="personal-picture" src="https://64.media.tumblr.com/e97a7387a98143108a3e0f81cc6115b4/7fc858227fd0b3f5-f0/s2048x3072/5d0011cebe9c925966b00183853ef9d3a1cff8bd.jpg" alt="Tainá" />
     </section>
   );
 }
