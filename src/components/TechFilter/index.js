cd0cd2e5989ac7975f4129837e185b43978c6914
Tainá Santana <tainnaps@gmail.com>
2022-04-20 04:27:15 -0300

    feat: add TechFilter component

---
+++ src/components/TechFilter/index.js
@@ -0,0 +1,29 @@
+import React, { useContext, useEffect } from 'react';
+import TechCheckbox from '../../components/TechCheckbox';
+import TechFilterContext from '../../context/TechFilterContext';
+import technologies from '../../data/technologies';
+
+function TechFilter() {
+  const { resetTechFilter } = useContext(TechFilterContext);
+
+  useEffect(() => {
+    resetTechFilter();
+  }, []);
+
+  const renderTechChecboxes = () => (
+    technologies.map(({ name }) => (
+      <TechCheckbox
+        key={ name }
+        name={ name }
+      />
+    ))
+  );
+
+  return (
+    <section className="tech-filter">
+      { renderTechChecboxes() }
+    </section>
+  );
+}
+
+export default TechFilter;
