0cd7fac29eaec51ba51e0b375a18822e51480306
Tainá Santana <tainnaps@gmail.com>
2022-04-22 06:32:29 -0300

    feat: implement Contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import ContactForm from '../../components/ContactForm';
+import Icon from '../../components/Icon';
+import Title from '../../components/Title';
 import './index.css';
 
 function Contact() {
   return (
-    <main className="contact-content">
-      <h1>In progress</h1>
+    <main className="contact-container">
+      <section className="contact-content">
+        <Title title="Contact" />
+        <section className="contact-info">
+          <h3>Want to contact me?</h3>
+          <p>Please, choose some of the channels below.</p>
+          <section className="contact-icons">
+            <Icon
+              href="https://t.me/tainnaps"
+              className="fa-brands fa-telegram"
+            />
+            <Icon
+              href="#contact-form-section"
+              blankTarget={ false }
+              className="fa-solid fa-envelope"
+            />
+            <Icon
+              href="https://wa.me/5571991360512"
+              className="fa-brands fa-whatsapp"
+            />
+          </section>
+        </section>
+      </section>
+      <section id="contact-form-section">
+        <Title title="Email Form" />
+        <ContactForm />
+      </section>
     </main>
   );
 }
