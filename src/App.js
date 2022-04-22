ed70fa4277159a11182596c6a4b28949321c417f
Tainá Santana <tainnaps@gmail.com>
2022-04-22 06:37:47 -0300

    style: remove commentaries

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import About from './pages/About';
 import NavBar from './components/NavBar';
-// import Technologies from './pages/Technologies';
 import Contact from './pages/Contact';
 import Footer from './components/Footer';
 import './App.css';
@@ -18,7 +17,6 @@ function App() {
         <Route exact path="/" component={ Home } />
         <Route exact path="/projects" component={ Projects } />
         <Route exact path="/about" component={ About } />
-        {/* <Route path="/technologies" component={ Technologies } /> */ }
         <Route exact path="/contact" component={ Contact } />
       </Switch>
       <Footer />
