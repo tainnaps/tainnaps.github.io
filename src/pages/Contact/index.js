79b531bebe517d5c2b4c26de952a92414f92eaa1
Tainá Santana <tainnaps@gmail.com>
2022-04-22 08:54:49 -0300

    feat: add contact card to contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -6,26 +6,28 @@ import './index.css';
 
 function Contact() {
   return (
-    <main className="contact-container">
-      <section className="contact-content">
-        <Title title="Contact" />
-        <section className="contact-info">
-          <h3>Want to contact me?</h3>
-          <p>Please, choose some of the channels below.</p>
-          <section className="contact-icons">
-            <Icon
-              href="https://t.me/tainnaps"
-              className="fa-brands fa-telegram"
-            />
-            <Icon
-              href="#contact-form-section"
-              blankTarget={ false }
-              className="fa-solid fa-envelope"
-            />
-            <Icon
-              href="https://wa.me/5571991360512"
-              className="fa-brands fa-whatsapp"
-            />
+    <main className="contact-main">
+      <section className="contact-container">
+        <section className="contact-card">
+          <Title title="Contact" />
+          <section className="contact-info">
+            <h3>Want to contact me?</h3>
+            <p>Please, choose some of the channels below.</p>
+            <section className="contact-icons">
+              <Icon
+                href="https://t.me/tainnaps"
+                className="fa-brands fa-telegram"
+              />
+              <Icon
+                href="#contact-form-section"
+                blankTarget={ false }
+                className="fa-solid fa-envelope"
+              />
+              <Icon
+                href="https://wa.me/5571991360512"
+                className="fa-brands fa-whatsapp"
+              />
+            </section>
           </section>
         </section>
       </section>
