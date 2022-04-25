dab0e081f75adbc9e784aaa5d68d6b3fe846912f
Tainá Santana <tainnaps@gmail.com>
2022-04-25 20:17:51 -0300

    style: remove fragment from return

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -4,24 +4,22 @@ import './index.css';
 
 function About() {
   return (
-    <>
-      <main className="about-content">
-        <Title title="About Me" />
-        <section className="description">
-          <p>
-            Hello! I am Tainá, a brazilian Web Software Developer.
-            My journey with programming has began throught electronics, when I used to write programs to microcontrollers using Assembly and C programming languages.
-          </p>
-          <p>
-            Now days, I build web applications using React.js to the front-end and Node.js to the back-end.
-            I am currently studying at Trybe and improving myself to develop applications that will be useful to other people.
-          </p>
-          <p>
-            Feel free to see the projects I have done and contact me. Have a nice time!
-          </p>
-        </section>
-      </main>
-    </>
+    <main className="about-content">
+      <Title title="About Me" />
+      <section className="description">
+        <p>
+          Hello! I am Tainá, a brazilian Web Software Developer.
+          My journey with programming has began throught electronics, when I used to write programs to microcontrollers using Assembly and C programming languages.
+        </p>
+        <p>
+          Now days, I build web applications using React.js to the front-end and Node.js to the back-end.
+          I am currently studying at Trybe and improving myself to develop applications that will be useful to other people.
+        </p>
+        <p>
+          Feel free to see the projects I have done and contact me. Have a nice time!
+        </p>
+      </section>
+    </main>
   );
 }
 
