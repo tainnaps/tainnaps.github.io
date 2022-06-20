a30633e8d333ae82a44a61bd4fb50ea39cf4d723
Tainá Santana <tainnaps@gmail.com>
2022-06-20 03:35:52 -0300

    feat: add projects and technologies

--- src/data/technologies.js
+++ src/data/technologies.js
@@ -1,71 +1,54 @@
 const technologies = [
   {
     name: 'HTML',
-    imgSrc: 'https://icons.iconarchive.com/icons/cornmanthe3rd/plex/256/Other-html-5-icon.png',
   },
   {
     name: 'CSS',
-    imgSrc: 'https://logospng.org/download/css-3/logo-css-3-2048.png',
   },
   {
     name: 'React',
-    imgSrc: 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png',
   },
   {
     name: 'Redux',
-    imgSrc: 'https://img.icons8.com/color/480/redux.png',
   },
   {
     name: 'JavaScript',
-    imgSrc: 'https://logospng.org/download/javascript/logo-javascript-icon-1024.png',
   },
   {
     name: 'Jest',
-    imgSrc: 'https://symbols.getvecta.com/stencil_25/40_jest.5fde12ec22.png',
   },
   {
     name: 'Mocha/Chai',
-    imgSrc: 'https://cldup.com/xFVFxOioAU.svg',
   },
   {
     name: 'Sinon',
-    imgSrc: 'https://avatars.githubusercontent.com/u/1515293?s=280&v=4',
+  },
+  {
+    name: 'React Native',
   },
   {
     name: 'Docker',
-    imgSrc: 'https://cdn-icons-png.flaticon.com/512/919/919853.png',
+  },
+  {
+    name: 'Sequelize',
   },
   {
     name: 'MySQL',
-    imgSrc: 'https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-images-are-download-crazypng-21.png',
+  },
+  {
+    name: 'TypeScript',
   },
   {
     name: 'Node',
-    imgSrc: 'https://walde.co/wp-content/uploads/2016/09/nodejs_logo.png',
   },
   {
     name: 'Express',
-    imgSrc: 'https://freepikpsd.com/file/2019/10/express-js-png-5-Transparent-Images.png',
   },
   // {
-  //   name: 'Heroku',
-  //   imgSrc: 'https://freepikpsd.com/file/2019/10/express-js-png-5-Transparent-Images.png',
-  // },
-  // {
-  //   name: 'TypeScript',
-  //   imgSrc: 'https://freepikpsd.com/file/2019/10/express-js-png-5-Transparent-Images.png',
-  // },
-  // {
-  //   name: 'Sequelize',
-  //   imgSrc: 'https://freepikpsd.com/file/2019/10/express-js-png-5-Transparent-Images.png',
-  // },
-  // {
   //   name: 'MongoDB',
-  //   imgSrc: 'https://freepikpsd.com/file/2019/10/express-js-png-5-Transparent-Images.png',
   // },
   // {
   //   name: 'Python',
-  //   imgSrc: 'https://freepikpsd.com/file/2019/10/express-js-png-5-Transparent-Images.png',
   // },
 ];
 
