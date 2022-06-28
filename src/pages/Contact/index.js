c373648e3c01d10950b479af1b06082eaa8a1c64
Tainá Santana <tainnaps@gmail.com>
2022-06-28 18:21:53 -0300

    refactor: render only title and form on contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ContactForm from '../../components/ContactForm';
-import Icon from '../../components/Icon';
 import Title from '../../components/Title';
 import './index.css';
 
@@ -8,31 +7,7 @@ function Contact() {
   return (
     <main className="contact-main">
       <section className="contact-container">
-        <section className="contact-card">
-          <Title title="Contact" />
-          <section className="contact-info">
-            <h3>Want to contact me?</h3>
-            <p>Please, choose some of the channels below.</p>
-            <section className="contact-icons">
-              <Icon
-                href="https://t.me/tainnaps"
-                className="fa-brands fa-telegram"
-              />
-              <Icon
-                href="#contact-form"
-                blankTarget={ false }
-                className="fa-solid fa-envelope"
-              />
-              <Icon
-                href="https://wa.me/5571991360512"
-                className="fa-brands fa-whatsapp"
-              />
-            </section>
-          </section>
-        </section>
-      </section>
-      <section id="contact-form">
-        <Title title="Email" />
+        <Title title="Contact" />
         <ContactForm />
       </section>
     </main>
